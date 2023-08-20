5c1a0adf14578b7ad94f2849ea9896f8b6e52a6e
eavila6 <ethan.alt128@gmail.com>
2023-08-19 18:14:10 -0700

    Small tweaks to try and resolve disappearing upon turning around
    Nothing really special, just reenabled a personally overloaded version of Raycast2D and tried to see if that might fix the issue

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -28,7 +28,7 @@ public class PlayerMovement : MonoBehaviour {
     public float footOffset = 0.4f;     // X offset of feet for raycasts
     public float headClearance = 0.5f;  // distance needed for head not to get hit
     public float grabDist = 0.4f;       // how far the player can reach
-    public float groundDist = 0.2f;     // fair game for being on the ground
+    public float groundDistance = 0.2f;     // fair game for being on the ground
     
     
     [Header ("Layer Mask")] public LayerMask groundLayer;            // layer of the ground
@@ -46,10 +46,12 @@ public class PlayerMovement : MonoBehaviour {
 	Vector2 colliderCrouchSize;				//Size of the crouching collider
 	Vector2 colliderCrouchOffset;			//Offset of the crouching collider
 
+    // DEBUG
+    /*
     [SerializeField]
     private GameObject leftRayOffset;
     [SerializeField]
-    private GameObject rightRayOffset;
+    private GameObject rightRayOffset;*/
 
     // Start is called before the first frame update
     void Start()
@@ -77,38 +79,36 @@ public class PlayerMovement : MonoBehaviour {
         //Start by assuming the player isn't on the ground and the head isn't blocked
 		grounded = false;
 		isHeadBlocked = false;
-        RaycastHit2D leftCheck;
-        RaycastHit2D rightCheck;
+        // RaycastHit2D leftCheck;
+        // RaycastHit2D rightCheck;
 
-        /*
+        
 		//Cast rays for the left and right foot
         RaycastHit2D leftCheck = Raycast(new Vector2(-footOffset, 0f), Vector2.down, groundDistance);
-		RaycastHit2D rightCheck = Raycast(new Vector2(footOffset, 0f), Vector2.down, groundDistance);*/
+		RaycastHit2D rightCheck = Raycast(new Vector2(footOffset, 0f), Vector2.down, groundDistance);
 
-		leftCheck = Physics2D.Raycast(leftRayOffset.transform.position, Vector2.down, groundDist, groundLayer);
-        rightCheck = Physics2D.Raycast(rightRayOffset.transform.position, Vector2.down, groundDist, groundLayer);
+		// leftCheck = Physics2D.Raycast(leftRayOffset.transform.position, Vector2.down, groundDist, groundLayer);
+        // rightCheck = Physics2D.Raycast(rightRayOffset.transform.position, Vector2.down, groundDist, groundLayer);
         // Debug.Log(LayerMask.GetMask("Ground"));
         // Debug.Log("Left:  " + leftCheck.collider);
         // Debug.Log("Right: " + rightCheck.collider);
 
 		//If either ray hit the ground, the player is on the ground
-		if (leftCheck.collider != null || rightCheck.collider != null)
-        {
+		/* if (leftCheck.collider != null || rightCheck.collider != null) {
             grounded = true;
-        }
-        /*
+        } */
+        
         //If either ray hit the ground, the player is on the ground
 		if (leftCheck || rightCheck) {
-			isOnGround = true;
-        }*/
+			grounded = true;
+        }
 
 
 		//Cast the ray to check above the player's head
 		RaycastHit2D headCheck = Physics2D.Raycast(new Vector2(0f, bodyCollider.size.y), Vector2.up, headClearance);
 
         //If that ray hits, the player's head is blocked
-        if (headCheck)
-        {
+        if (headCheck) {
             isHeadBlocked = true;
         }
     }
@@ -220,7 +220,7 @@ public class PlayerMovement : MonoBehaviour {
 
     // these two methods are from the tutorial I've been following for player movement
     // I'm testing them out to see if they're any good
-    /*
+    
     RaycastHit2D Raycast(Vector2 offset, Vector2 rayDirection, float length)
     {
         //Call the overloaded Raycast() method using the ground layermask and return 
@@ -248,5 +248,5 @@ public class PlayerMovement : MonoBehaviour {
         //Return the results of the raycast
         return hit;
     }
-    */
+    
 }
